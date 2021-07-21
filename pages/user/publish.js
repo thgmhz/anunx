84af5a031d8c722803daf3dfb672b28edfcfdc82
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-07-21 10:45:59 -0300

    adicionando feature de upload de imagens (front)

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,9 +1,23 @@
-import { Container, Typography, Box, TextField, Select, Button } from '@material-ui/core'
+import { useState } from 'react'
+import {
+  Container,
+  Typography,
+  Box,
+  TextField,
+  Select,
+  Button,
+  IconButton
+} from '@material-ui/core'
+
+import { useDropzone } from 'react-dropzone'
 import { makeStyles } from '@material-ui/core/styles'
+import { DeleteForever } from '@material-ui/icons'
 
 import TemplateDefault from '../../src/templates/Default'
 
 const useStyles = makeStyles((theme) => ({
+  mask: {},
+  mainImage: {},
   container: {
     padding: theme.spacing(8, 0, 6)
   },
@@ -13,11 +27,80 @@ const useStyles = makeStyles((theme) => ({
   box: {
     backgroundColor: theme.palette.background.white,
     padding: theme.spacing(3),
+  },
+  thumbsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: 15,
+  },
+  dropzone: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,    
+    width: 200,
+    height: 150,
+    margin: '0 15px 15px 0',
+    backgroundColor: theme.palette.background.default,
+    border: '2px dashed black'
+  },
+  thumb: {
+    position: 'relative',
+    width: 200,
+    height: 150,
+    backgroundSize: 'cover',
+    margin: '0 15px 15px 0',
+    backgroundPosition: 'center center',
+
+    '& $mainImage': {
+      backgroundColor: 'blue',
+      padding: '6px 10px',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+    },
+
+    '&:hover $mask': {
+      display: 'flex',
+    },
+
+    '& $mask': {
+      display: 'none',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      width: '100%',
+      height: '100%'
+    }
   }
 }))
 
 const Publish = () => {
   const classes = useStyles()
+  const [files, setFiles] = useState([])
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFile) => {      
+      const newFiles = acceptedFile.map(file => {        
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })
+      })
+
+      setFiles([
+        ...files,
+        ...newFiles,
+      ])
+    }
+  })
+
+  const handleRemoveFile = fileName => {
+    const newFileState = files.filter(file => file.name !== fileName)
+    setFiles(newFileState)
+  }
 
   return (
     <TemplateDefault>
@@ -81,6 +164,39 @@ const Publish = () => {
           <Typography component="div" variant="body2" color="textPrimary">
             A primeira imagem é a foto principal do seu anúncio.
           </Typography>
+          <Box className={classes.thumbsContainer}>
+            <Box className={classes.dropzone} {...getRootProps()}>
+              <input {...getInputProps()} />              
+              <Typography variant="body2" color="textPrimary">
+                Clique para adicionar ou arraste a imagem para aqui.
+              </Typography>
+            </Box>
+            
+            {
+              files.map((file, index) => (
+                <Box
+                  key={file.name}
+                  className={classes.thumb}
+                  style={{ backgroundImage: `url(${file.preview})` }}
+                >
+                  {
+                    index === 0 ?
+                      <Box className={classes.mainImage}>
+                        <Typography variant="body" color="secondary">
+                          Principal
+                        </Typography>
+                      </Box> 
+                    : null
+                  }                  
+                  <Box className={classes.mask}>
+                    <IconButton color="secondary" onClick={() => handleRemoveFile(file.name)}>
+                      <DeleteForever fontSize="large" />
+                    </IconButton>
+                  </Box>
+                </Box>
+              ))
+            }            
+          </Box>
         </Box>
       </Container>
 
--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,9 @@ const theme = createTheme({
     primary: {
       main: '#000000',
     },
+    secondary: {
+      main: '#ffffff',
+    },
     background: {
       default: 'rgb(242, 244, 245)',
       white: '#ffffff'
