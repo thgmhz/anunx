f43c001f5402d2c699c3ac5364639927374ec61b
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-07-26 16:22:36 -0300

    validacao do campo categoria

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -14,6 +14,8 @@ import {
   InputLabel,
   OutlinedInput,
   InputAdornment,
+  MenuItem,
+  FormHelperText,
 } from '@material-ui/core'
 
 import { useDropzone } from 'react-dropzone'
@@ -85,7 +87,8 @@ const validationSchema = yup.object().shape({
   title: yup.string()
     .min(6, 'Escreva um título maior')
     .max(100, 'Título muito grande')
-    .required('Campo obrigatório'),  
+    .required('Campo obrigatório'), 
+  category: yup.string().required('Campo obrigatório')
 })
 
 const Publish = () => {
@@ -118,7 +121,8 @@ const Publish = () => {
     <TemplateDefault>
       <Formik
         initialValues={{
-          title: ''
+          title: '',
+          category: '',
         }}
         validationSchema={validationSchema}
         onSubmit={(values) => {
@@ -166,32 +170,33 @@ const Publish = () => {
                     <Typography component="h6" variant="h6" color="textPrimary">
                       Categoria
                     </Typography>
-                    <Select
-                      native
-                      value=""
-                      fullWidth
-                      onChange={() => {}}
-                      inputProps={{
-                        name: 'age',
-                      }}
-                    >
-                      <option value="">Selecione</option>
-                      <option value={1}>Bebê e Criança</option>
-                      <option value={2}>Agricultura</option>
-                      <option value={3}>Moda</option>
-                      <option value={3}>Carros, Motos e Barcos</option>
-                      <option value={3}>Serviços</option>
-                      <option value={3}>Lazer</option>
-                      <option value={3}>Animais</option>
-                      <option value={3}>Moveis, Casa e Jardim</option>
-                      <option value={3}>Imóveis</option>
-                      <option value={3}>Equipamentos e Ferramentas</option>
-                      <option value={3}>Celulares e Tablets</option>
-                      <option value={3}>Esporte</option>
-                      <option value={3}>Tecnologia</option>
-                      <option value={3}>Emprego</option>
-                      <option value={3}>Outros</option>
-                    </Select>
+                    <FormControl error={errors.category} fullWidth>
+                      <Select
+                        name="category"
+                        value={values.category}
+                        fullWidth
+                        onChange={handleChange}                      
+                      >                        
+                        <MenuItem value="Bebê e Criança">Bebê e Criança</MenuItem>
+                        <MenuItem value="Agricultura">Agricultura</MenuItem>
+                        <MenuItem value="Moda">Moda</MenuItem>
+                        <MenuItem value="Carros, Motos e Barcos">Carros, Motos e Barcos</MenuItem>
+                        <MenuItem value="Serviços">Serviços</MenuItem>
+                        <MenuItem value="Lazer">Lazer</MenuItem>
+                        <MenuItem value="Animais">Animais</MenuItem>
+                        <MenuItem value="Moveis, Casa e Jardim">Moveis, Casa e Jardim</MenuItem>
+                        <MenuItem value="Imóveis">Imóveis</MenuItem>
+                        <MenuItem value="Equipamentos e Ferramentas">Equipamentos e Ferramentas</MenuItem>
+                        <MenuItem value="Celulares e Tablets">Celulares e Tablets</MenuItem>
+                        <MenuItem value="Esporte">Esporte</MenuItem>
+                        <MenuItem value="Tecnologia">Tecnologia</MenuItem>
+                        <MenuItem value="Emprego">Emprego</MenuItem>
+                        <MenuItem value="Outros">Outros</MenuItem>
+                      </Select>
+                      <FormHelperText>
+                        { errors.category }
+                      </FormHelperText>
+                    </FormControl>
                   </Box>
                 </Container>
               
