d8617fa08d6f292d95f65452e1e6c5b2ed433cf8
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-07-30 13:56:41 -0300

    adicionando APP_URL no .env.local

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export default NextAuth({
     Providers.Credentials({
       name: 'Credentials',
       async authorize(credentials) {
-        const res = await axios.post('http://localhost:3000/api/auth/signin', credentials)        
+        const res = await axios.post(`${process.env.APP_URL}/api/auth/signin`, credentials)        
 
         const user = res.data        
 
--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -22,7 +22,7 @@ import useToasty from '../../../src/contexts/Toasty'
 import useStyles from './styles'
 import Alert from '@material-ui/lab/Alert'
 
-const Signin = () => {
+const Signin = ({ APP_URL }) => {
   const classes = useStyles()
   const router = useRouter()
   const { setToasty } = useToasty()
@@ -32,7 +32,7 @@ const Signin = () => {
 
   const handleGoogleLogin = () => {
     signIn('google', {
-      callbackUrl: 'http://localhost:3000/user/dashboard'
+      callbackUrl: `${APP_URL}/user/dashboard`
     })
   }
 
@@ -40,7 +40,7 @@ const Signin = () => {
     signIn('credentials', {
       email: values.email,
       password: values.password,
-      callbackUrl: 'http://localhost:3000/user/dashboard'
+      callbackUrl: `${APP_URL}/user/dashboard`
     })
   }
 
@@ -153,6 +153,12 @@ const Signin = () => {
       </Container>
     </TemplateDefault>
   )
+}
+
+Signin.getInitialProps = async function() {
+  return {
+    APP_URL: process.env.APP_URL
+  }
 }
 
 export default Signin
