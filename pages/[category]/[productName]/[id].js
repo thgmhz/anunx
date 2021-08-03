94b25e638f40aef7458fed67b14f079c130039d4
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-08-03 17:08:52 -0300

    integrando pagina de produto com api

--- pages/[category]/[productName]/[id].js
+++ pages/[category]/[productName]/[id].js
@@ -13,7 +13,10 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 import Carousel from 'react-material-ui-carousel'
 
-import TemplateDefault from '../../src/templates/Default'
+import TemplateDefault from '../../../src/templates/Default'
+import ProductsModel from '../../../src/models/products'
+import dbConnect from '../../../src/utils/dbConnect'
+import { formatCurrency }  from '../../../src/utils/currency'
 
 const useStyles = makeStyles((theme) => ({
   box: {
@@ -36,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const Product = () => {
+const Product = ({ product }) => {
   const classes = useStyles()
 
   return (
@@ -55,34 +58,31 @@ const Product = () => {
                   }
                 }}         
               >
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random?a=1"
-                    title="Título da imagem"
-                  />
-                </Card>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random?a=2"
-                    title="Título da imagem"
-                  />
-                </Card>
+                {
+                  product.files.map(file => (
+                    <Card key={file.name} className={classes.card}>
+                      <CardMedia
+                        className={classes.cardMedia}
+                        image={`/uploads/${file.name}`}
+                        title={product.title}
+                      />
+                    </Card>
+                  ))
+                }                                
               </Carousel>
             </Box>
 
             <Box className={classes.box} textAlign="left">
-              <Typography component="span" variant="caption">Publicado 16 junho de 2021</Typography>
-              <Typography component="h4" variant="h4" className={classes.productName}>Jaguar XE 2.0 D R-Sport Aut.</Typography>
-              <Typography component="h4" variant="h4" className={classes.price}>R$ 50.000,00</Typography>
-              <Chip label="Categoria" />
+              <Typography component="span" variant="caption">Publicado 16 junho de 2021 -- TO DO</Typography>
+              <Typography component="h4" variant="h4" className={classes.productName}>{product.title}</Typography>
+              <Typography component="h4" variant="h4" className={classes.price}>{formatCurrency(product.price)}</Typography>
+              <Chip label={product.category} />
             </Box>
 
             <Box className={classes.box} textAlign="left">              
               <Typography component="h6" variant="h6">Descrição</Typography>
               <Typography component="p" variant="body2">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                {product.description}
               </Typography>              
             </Box>
           </Grid>
@@ -91,14 +91,16 @@ const Product = () => {
             <Card elevation={0} className={classes.box}>
               <CardHeader
                 avatar={
-                  <Avatar>T</Avatar>
+                  <Avatar src={product.user.image}>
+                    { product.user.image || product.user.name[0] }
+                  </Avatar>
                 }
-                title="Thiago Medeiros"
-                subheader="thiago@email.com"
+                title={product.user.name}
+                subheader={product.user.email}
               />
               <CardMedia
-                image="https://source.unsplash.com/random"
-                title="Thiago Medeiros"
+                image={product.user.image}
+                title={product.user.name}
               />
             </Card>
 
@@ -112,6 +114,21 @@ const Product = () => {
       </Container>
     </TemplateDefault>
   )
+}
+
+
+export async function getServerSideProps({ query }) {
+  const { id } = query
+
+  await dbConnect()
+
+  const product = await ProductsModel.findOne({ _id: id })
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product))
+    }    
+  }
 }
 
 export default Product
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -34,8 +34,8 @@ const post = async (req, res) => {
 
       const filename = `${timestamp}_${random}${extension}`
 
-      const oldpath = path.join(__dirname, `../../../../${file.path}`)
-      const newpath = path.join(__dirname, `../../../../${form.uploadDir}/${filename}`)
+      const oldpath = path.join(__dirname, `../../../../../${file.path}`)
+      const newpath = path.join(__dirname, `../../../../../${form.uploadDir}/${filename}`)
 
       filesToSave.push({
         name: filename,
